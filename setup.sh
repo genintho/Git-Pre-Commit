1b60e59419a053fecdc764620b2b491454cb4a9f
Thomas Genin <xt6.thomas.genin@gmail.com>
2013-04-17 22:02:40 -0700

    Add setup file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#s!/bin/sh
+
+echo "install start"
+echo "Create Dev Tool Dir"
+mkdir ~/DevTools
+echo "Fetch Git Repo"
+git clone git@github.com:genintho/Git-Pre-Commit.git
+cd Git-Pre-Commit
+"Init git commit"
+ln ./pre-commit.sh /expensify/staging/www/git/expensify.com/.git/hooks/pre-commit
+cd -
+echo "install is over"
+
