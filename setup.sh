1170c07c47d4a9fcb08435051cb86e621487c34a
Thomas Genin <xt6.thomas.genin@gmail.com>
2013-04-17 22:10:07 -0700

    fix directory typo

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 echo "install start"
 echo "Create Dev Tool Dir"
 mkdir ~/DevTools
+cd DevTools
 echo "Fetch Git Repo"
 git clone git@github.com:genintho/Git-Pre-Commit.git
-cd Git-Pre-Commit
 "Init git commit"
 ln ./pre-commit.sh /expensify/staging/www/git/expensify.com/.git/hooks/pre-commit
 cd -
